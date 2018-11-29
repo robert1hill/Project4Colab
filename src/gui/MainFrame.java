ec844654670390d3cd3d93b311d30b6550539d38
Bob Underhill <roberthill@ou.edu>
2018-11-29 10:58:38 -0600

    put in code from class exercise

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,34 @@
+package gui;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame
+{
+    private TextPanel textPanel;
+    private Toolbar toolbar;
+    private TextPanel tp;
+    
+    public MainFrame()
+    {
+        super("hello");
+        
+        setLayout(new BorderLayout());
+        toolbar = new Toolbar();
+        textPanel = new TextPanel();
+        tp = new TextPanel();
+        
+        toolbar.setTextPanel(textPanel);
+        
+        add(toolbar, BorderLayout.NORTH);
+        add(textPanel, BorderLayout.CENTER);
+        add(tp, BorderLayout.SOUTH);
+        
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        
+    }
+    
+}
---
+++ src/gui/Toolbar.java
@@ -0,0 +1,51 @@
+package gui;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Toolbar extends JPanel implements ActionListener
+{
+    private JButton startButton;
+    private JButton endButton;
+    private TextPanel textPanel;
+    
+    public Toolbar()
+    {
+        startButton = new JButton("Start");
+        endButton = new JButton("terminate");
+    
+        startButton.addActionListener(this);
+        endButton.addActionListener(this);
+        
+        add(startButton);
+        add(endButton);
+        
+    }
+    
+    public void setTextPanel(TextPanel textPanel)
+    {
+        this.textPanel = textPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        //this tells me what button was clicked
+        JButton clicked = (JButton)e.getSource();
+        
+        if (clicked == startButton)
+        {
+            textPanel.appendText("Start your imagination" + '\n');
+        }
+        else if (clicked == endButton)
+        {
+            textPanel.appendText("Keep dreaming" + '\n');
+        }
+        
+    }
+    
+    
+    
+}
